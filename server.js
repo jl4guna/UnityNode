a47792002de73a427ca5b137da90881df6c97112
lagunaisw <jhonlpdev@gmail.com>
2016-08-18 13:25:18 -0700

    Basic broadcast

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 'use strict';
+//Inicia el servidor
 const io = require('socket.io')(process.env.PORT || 3000);
 
 console.log('server starter');
+//Escucha las conexiones
 io.on('connection', function (socket) {
-  console.log('client connected');
+  console.log('client connected, broadcasting spawn');
 
+  //Notifica a todos los clientes conectados
+  socket.broadcast.emit("spawn");
+
+  //Escucha el evento move del cliente
   socket.on('move', function (data) {
     console.log("Client moved!");
   });
