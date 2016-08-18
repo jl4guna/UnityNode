6cb36ba721b9430a5e00af2909e8659ac3e5bb39
lagunaisw <jhonlpdev@gmail.com>
2016-08-18 14:05:47 -0700

    Showing others players and disconeccting

--- server.js
+++ server.js
@@ -3,16 +3,33 @@
 const io = require('socket.io')(process.env.PORT || 3000);
 
 console.log('server starter');
+
+var playerCount = 0;
+
 //Escucha las conexiones
 io.on('connection', function (socket) {
   console.log('client connected, broadcasting spawn');
 
   //Notifica a todos los clientes conectados
   socket.broadcast.emit("spawn");
+  //Aumentamos el numero de usuarios
+  playerCount++;
+
+  //Mostramos a las nuevas conexiones todos los usuarios que ya estan conectados
+  for (var i = 0; i < playerCount; i++) {
+    socket.emit('spawn');
+    console.log('sending spawn to new player');
+  }
 
   //Escucha el evento move del cliente
   socket.on('move', function (data) {
     console.log("Client moved!");
   });
+
+  //Escucha las desconexiones
+  socket.on('disconnect', function () {
+    console.log('client disconnected');
+    playerCount--;
+  })
 
 });
