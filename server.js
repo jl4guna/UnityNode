21966ae7998b7e18f4a4e34b5e2baefd11760695
lagunaisw <jhonlpdev@gmail.com>
2016-08-19 12:54:20 -0700

    Refactor: players connected

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const shortid = require('shortid');
 
 console.log('server starter');
 
-var playerCount = 0;
+var players = [];
 
 //Escucha las conexiones
 io.on('connection', function (socket) {
@@ -13,18 +13,21 @@ io.on('connection', function (socket) {
   //Generamos un identificador unico para esta conexion
   const thisClientId = shortid.generate();
 
+  //Añadimos al nuevo jugador a la lista
+  players.push(thisClientId);
+
   console.log('client connected, broadcasting spawn, id:', thisClientId);
 
   //Notifica a todos los clientes conectados
-  socket.broadcast.emit("spawn");
-  //Aumentamos el numero de usuarios
-  playerCount++;
+  socket.broadcast.emit("spawn", { id: thisClientId });
 
-  //Mostramos a las nuevas conexiones todos los usuarios que ya estan conectados
-  for (var i = 0; i < playerCount; i++) {
-    socket.emit('spawn');
-    console.log('sending spawn to new player');
-  }
+  //Emite la lista de jugadores conectados
+  players.forEach(function(playerId){
+    if(playerId === thisClientId)
+      return;
+    socket.emit('spawn', {id: playerId});
+    console.log('Sending spawn to new player for id: ' + playerId);
+  });
 
   //Escucha el evento move del cliente
   socket.on('move', function (data) {
@@ -37,7 +40,6 @@ io.on('connection', function (socket) {
   //Escucha las desconexiones
   socket.on('disconnect', function () {
     console.log('client disconnected');
-    playerCount--;
   })
 
 });
